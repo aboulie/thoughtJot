75a077553200808c0ce3e622fd2d201a91325e58
Keren Shimoni <kerenshimoni6@gmail.com>
2018-07-06 17:48:48 -0700

    route to grab data

--- routes/grabPosts.js
+++ routes/grabPosts.js
@@ -8,21 +8,19 @@ const app = express();
 
 // app.use(cors());
 //GRAB POSTS
+//checks if a user is logged in...if logged in, then show all enteries 
 router.post('/grabposts', function(request, response){
     if(request.user){ //user is a passport object ...if our req gives response that has a user
         db.UserData.findAll({
-            where: {userId: request.user.id}
-        }).then(function(data){
-            //
-        })
-    }
+			where: {userId: request.user.id}
+		}).then(function(data){
+			response.json(data);
+		})
+	} else {
+		response.status(401);
+		response.json({message: "not logged in"})
+	}
 })
 
+module.exports = router;
 
-
-//checks if a user is logged in...if logged in, then show all enteries 
-
-
-//showing all the data
-
-// /logedIn
